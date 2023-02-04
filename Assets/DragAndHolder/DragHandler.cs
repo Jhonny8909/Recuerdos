c387e4c6eb7fcfe76c144164c1ce9ffa09acb3ea
Jhonny8909 <cdmx2508@amerike.edu.mx>
2023-02-04 02:14:17 -0600

    DragAndDrop

--- Assets/DragAndHolder/DragHandler.cs
+++ Assets/DragAndHolder/DragHandler.cs
@@ -3,33 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragHandler : MonoBehaviour, IDragHandler,IEndDragHandler,IBeginDragHandler
+public class DragHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
 {
+    public static GameObject itemDragging;
+
+    Vector3 startPosition;
+    Transform startParent;
+    Transform dragParent;
+
+    void Start()
+    {
+        dragParent = GameObject.FindGameObjectWithTag("DragParent").transform;
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
+        itemDragging = gameObject;
+
+        startPosition = transform.position;
+        startParent = transform.parent;
+        transform.SetParent(dragParent);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
         transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
-    }
+        itemDragging = null;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (transform.parent == dragParent)
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent);
+        }
     }
 }
